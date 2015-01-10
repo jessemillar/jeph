dd8bc7b51f96c8216be0f7220ed3a41845599b72
Jesse Millar <hellojessemillar@gmail.com>
2015-01-09 18:26:25 -0700

    Working on a shell script

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+OUTPUT="database.json"
+
+if [ -f database.json ]
+then
+	rm database.json
+fi
+
+echo "{" >> $OUTPUT
+
+for i in "images"/*; do
+	echo "    \"$(basename "$i")\": {" >> $OUTPUT # Make the directories JSON objects
+
+	for j in "$i"/*; do
+		echo "        \"$(basename "$j")\"," >> $OUTPUT # Make each image a child of the object
+	done
+
+	echo "    }," >> $OUTPUT
+done
+
+echo "}" >> $OUTPUT
