5b4ba215812b10da520f757b81903a58cadb3a77
Jesse Millar <hellojessemillar@gmail.com>
2015-01-10 17:00:27 -0700

    URL parameters work

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,45 @@ var random = function(cap)
 	return Math.floor(Math.random() * cap)
 }
 
-var load_gif_previews = function() // Runs on page load
+function getParameter()
 {
-	// if (special_url)
-	// {
+	if (location.href.indexOf("#") > -1) // If there is a parameter
+	{
+		var parameter = location.href.substring(location.href.indexOf("#") + 1, location.href.length)
 
-	// }
-	// else
-	// {
-		for (var i = 0; i < categories.length; i++)
+		if (categories.indexOf(parameter) > -1)
 		{
-			make_preview(i, categories[i], gifs[i][random(gifs[i].length)])
+			return categories.indexOf(parameter)
 		}
-	// }
+		else
+		{
+			console.log('Error') // This is lazy and I'll make it do something better later
+		}
+	}
+	else
+	{
+		return false
+	}
+}
+
+var init = function() // Runs on page load
+{
+	if (getParameter())
+	{
+		load_gif_category(getParameter())
+	}
+	else
+	{
+		load_gif_previews()
+	}
+}
+
+var load_gif_previews = function()
+{
+	for (var i = 0; i < categories.length; i++)
+	{
+		make_preview(i, categories[i], gifs[i][random(gifs[i].length)])
+	}
 }
 
 var make_preview = function(index, category, gif)
@@ -23,7 +49,7 @@ var make_preview = function(index, category, gif)
 	var ul = document.getElementById('gifs')
 	var li = document.createElement('li')
 
-	li.onclick = function() { load_gif_category(index) }
+	li.onclick = function() { clear_ul(); location.href += '#' + category; load_gif_category(index);  }
 	li.innerHTML = "<div class = 'title'>" + category + "</div>"
 	li.style.backgroundImage = "url('gifs/" + category + "/" + gif + "')"
 	ul.appendChild(li)
@@ -31,10 +57,6 @@ var make_preview = function(index, category, gif)
 
 var load_gif_category = function(index)
 {
-	clear_ul()
-
-	console.log(gifs[index])
-
 	for (var i = 0; i < gifs[index].length; i++)
 	{
 		show_gif(categories[index], gifs[index][i])
