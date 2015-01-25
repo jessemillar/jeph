086a911933ccbd4e11ec0d2fc0cd484773f4a63b
Jesse Millar <hellojessemillar@gmail.com>
2015-01-25 10:51:01 -0700

    Better mobile loading

--- scripts/main.js
+++ scripts/main.js
@@ -52,10 +52,8 @@ var loadMobileGifs = function() // Automatically load GIF previews if in the fra
 		var rect = li.getBoundingClientRect()
 
 		return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+			rect.bottom >= 0 &&
+			rect.top <= (window.innerHeight || document.documentElement.clientHeight) /*or $(window).height() */
 		)
 	}
 
